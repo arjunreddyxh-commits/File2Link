1429b9bfb62eb250f317294a4db1e917df26af3f
arjunreddyxh-commits <arjunreddyxh@gmail.com>
2025-10-23 12:30:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,7 +4,7 @@ from os import environ
 id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
-SESSION = environ.get('SESSION', 'hemanthxrohith')
+SESSION = environ.get('SESSION', 'BQGNdJQAY3vnC14LispEuimf6lWS0nFFbbv3jjbWLtdjAb2I-AkOScEtmX_GfCueYW3Zl_klzFzPopWeQMlOdo1gQ8HFAvLL9JKIboy5jRZ6dTmlrAtiV_9rk--CsVWGzr-i6craL0s8bo6Zbo6Lde5Xhik7Lq-qJs6vrOt-Kl-P7tJUdtkaH3M9FhMxa5Rv_WCd2zMFJOhxP-aoARqMGca1XVMKlrvUPoJxWmplSvpNR7_6x0T-XdRP5scvHGirit3hZ6Tr5-nef2PmupM0VTgw5N1bkYnOGnNmQbc_RnP1IJvqToH-cCJW3Bx2DXBHko8dw70BBoVp5X5w_nYA6kJoGJFNXgAAAAGvczYoAA')
 API_ID = int(environ.get('API_ID', '20232047'))
 API_HASH = environ.get('API_HASH', 'ec4fab92f1d53f4b62db607225d994ad')
 BOT_TOKEN = environ.get('BOT_TOKEN', "8357746722:AAGOy2I9Y7oN7Q4l3p_bShxIuqEu9vC-hz0")
